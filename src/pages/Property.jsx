a0f7c17ff7fdb75296d275ad3c068a62073634c4
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2024-12-14 08:33:34 +0100

    populated the dummy data for the property lissting page

--- src/pages/Property.jsx
+++ src/pages/Property.jsx
@@ -1,11 +1,11 @@
 import { properties } from "../assets/assets"
-
+// import { Link } from "react-router-dom";
 function Property() {
   return (
     <div className="container flex flex-col mx-auto items-center py-4 pt-20 px-6 w-full overflow-hidden ">
       <h2 className="text-2xl mb-2">  Hot Properties For Sale</h2>
       <p className="text-center mb-8 max-w-80">Check out the latest  properties listed  for sale</p>
-      <div className="overflow-hidden">
+      {/* <div className="overflow-hidden">
         <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(320px,_1fr))]">
           {properties.map((property, index) => (
             <div className=""
@@ -22,10 +22,23 @@ function Property() {
             </div>
           ))}
         </div>
+      </div> */}
+
+      <div className='grid grid-cols-2 sm:grid-co ls-3 md:grid-cols-4 lg:grid-col-5 gap-4 gap-y-6'>
+        {properties.map((property, index) => (
+          <div key={index} >
+            <img src={property.image} 
+                 alt={property.title} 
+                 className="rounded-sm w-full h-full"/>
+            <h2 className="text-xl font-poppins">{property.title} </h2>
+            <p className="text-sm">  {property.price} <span className='text-sm'> {property.area} </span> </p>
+          </div> 
+        ))}
       </div>
 
-    </div>
+
+    </div> 
   )
 }
 
-export default Property
+export default Property;
