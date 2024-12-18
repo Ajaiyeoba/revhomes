118622d4b4ce4a439a7660924db6b76562464ee2
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2024-12-18 21:50:30 +0100

    checking

---
+++ src/admin/components/Navbar.jsx
@@ -0,0 +1,82 @@
+import { HomeIcon, MenuIcon, XIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+  useEffect(() => {
+    if (setOpenMenu) {
+      document.body.overflow = "hidden";
+    } else {
+      document.body.overflow = "auto";
+    }
+    return () => {
+      document.body.overflow = "auto";
+    };
+  }, [setOpenMenu]);
+
+  return (
+    <div className="absolute top-0 left-0 w-full z-10 bg-gray-900 ">
+      <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
+        <h2 className="text-white font-rubik flex">
+          {" "}
+          <HomeIcon className="text-blue-600" />
+          revHomes
+        </h2>
+        <ul className=" hidden md:flex gap-7 font-poppins text-white">
+          <Link to="/">
+            <p className="hover:text-gray-400 cursor-pointer">Home</p>
+          </Link>
+
+          <Link to="/about">
+            <p className="hover:text-gray-400 cursor-pointer">About</p>
+          </Link>
+
+          <Link to='/property'>
+            <p className="hover:text-gray-400 cursor-pointer">Properties</p>
+          </Link>
+
+         
+        </ul>
+
+        <button className="hidden md:block font-poppins bg-white px-8 py-2 rounded-full">
+          Admin
+        </button>
+
+        <MenuIcon
+          className="md:hidden w-8 text-white"
+          onClick={() => setOpenMenu(true)}
+        />
+      </div>
+      {/* Mobile Menu */}
+      <div
+        className={`md:hidden ${
+          openMenu ? "fixed w-full" : "h-0 w-0"
+        }   right-0 top-0 bottom-0 overflow-hidden bg-white transition-all `}
+      >
+        <div className="justify-end flex cursor-pointer p-6">
+          <XIcon className="w-8  " onClick={() => setOpenMenu(false)} />
+        </div>
+
+        <ul className="flex flex-col  items-center gap-2 mt-5 px-5 text-lg font-medium  ">
+          <Link to="/" onClick={() => setOpenMenu(false)}>
+            <p className="px-4 py-2 rounded-full inline-block ">Home</p>
+          </Link>
+
+          <Link to='/about' onClick={() => setOpenMenu(false)}>
+            <p className="px-4 py-2 rounded-full inline-block ">About</p>
+          </Link>
+
+          <Link to='/property' onClick={() => setOpenMenu(false)}>
+            <p className="px-4 py-2 rounded-full inline-block ">Properties</p>
+          </Link>
+
+
+
+
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
