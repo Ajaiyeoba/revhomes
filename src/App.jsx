5e98a20bc4f247e5ad7aad2ec811cbaef8ad1710
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2024-12-20 15:02:32 +0100

    fixed the routing system to diff btw clientside and the admin side

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { ToastContainer } from "react-toastify";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Property from "./pages/Property";
 import AdminHome from './admin/pages/Home';
+import AddProperty from './admin/pages/AddProperty';
 function App() {
+
+  const location = useLocation();
+
+  const isAdminRoute = location.pathname.startsWith('/admin');
+
   return (
     <div className="w-full overflow-hidden">
       <ToastContainer />
-      <Navbar />
-
+      {/* Show Navbar only on Clientside */}
+      {!isAdminRoute && <Navbar />}
       <Routes>
+
+        {/* Client Side routes */}
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/property" element={<Property />}/>
-        <Route path="/admin" element={<AdminHome />}/>
+        <Route path="/property" element={<Property />} />
+
+        {/* Admin Routes */}
+
+        <Route path="/admin" element={<AdminHome />} />
+        <Route path='/add-property' element={<AddProperty />}/>
       </Routes>
 
-      <Footer />
+      {!isAdminRoute && <Footer />}
     </div>
   );
 }
